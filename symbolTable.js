97e7119c264f8205eaaa01d8c2f270e13e2f0f31
knownastron <knownastron@gmail.com>
2017-11-28 14:03:55 -0800

    added myMethod in keyValue class

--- symbolTable.js
+++ symbolTable.js
@@ -4,8 +4,8 @@ class keyValue {
             [key] : value
         };
     }
-    toString() {
-        console.log(this.pair);
+    myMethod() {
+        return this.pair[key] * 5;
     }
 }
 
@@ -21,34 +21,21 @@ class ST {
     }
 
     print() {
-        let counter = 1;
         for (let i = 0; i < this.st.length; i++) {
-            console.dir(this.st[i].toString());
+            console.log(this.st[i].toString());
         }
     }
-
-    get(key) {
-    }
-
-    delete(key) {
-    }
-
-    contains(key) {
-    }
-
-    size() {
-    }
-
 }
+
 
 
 let myST = new ST();
 
 myST.add('Wu-Tang Clan', 'Don\'t fuck with.');
-//myST.add('G-Unit', 'Wack ass early 2000s gangster rap group.');
+myST.add('G-Unit', 'Wack ass early 2000s gangster rap group.');
 //myST.add('NWA', 'The father of gangsta rap.');
 //myST.add('Wu-Tang Clan', 'From the streets of Shaolin');
 
-myObject = new keyValue('ya', 'what');
-//console.log(myObject);
-myST.print();
+myObject = new keyValue(5, 10);
+console.log(myObject.myMethod());
+//myST.print();
